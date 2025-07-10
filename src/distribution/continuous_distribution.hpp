f3c98b18876907bbddb40390610ecd30d45d9315
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-07-10 19:04:29 +0300

    Add a new file 'continuous_distribution.hpp'

---
+++ src/distribution/continuous_distribution.hpp
@@ -0,0 +1,192 @@
+/*
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#             CALCULATE PDF AND CDF FOR A CONTINUOUS DISTRIBUTION              #
+#                                                                              #
+# Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
+################################################################################
+*/
+# pragma	once
+# include	"cumulative_function.hpp"
+
+//****************************************************************************//
+//      Class "ContinuousDistribution"                                        //
+//****************************************************************************//
+class ContinuousDistribution
+{
+//============================================================================//
+//      Members                                                               //
+//============================================================================//
+private:
+	Range range;						// Values range
+	vector <double> values;				// Unique values
+	vector <double> pdf;				// Computed values of a PDF function
+	vector <double> cdf;				// Computed values of a CDF function
+
+//============================================================================//
+//      Public methods                                                        //
+//============================================================================//
+public:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Default constructor                                                   //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	ContinuousDistribution (void)
+	:	range (Range (0, 0))
+	{}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructors for empirical data                                       //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	ContinuousDistribution (
+		const vector <double> &data,	// Empirical dataset
+		size_t bins						// Bins count for a histogram
+	) : range (Range (min (data), max (data)))
+	{
+		// Split the range into sub-ranges (bins)
+		values = range.Split (bins);
+
+		// Compute raw CDF
+		vector <double> raw_values;
+		vector <double> raw_cdf;
+		if (compute_cdf (data, raw_values, raw_cdf)) {
+
+			// Compute the PDF and CDF functions using created bins
+			// We do linear interpolation when a value resides between
+			// two empirical points
+			double last = raw_cdf [0];
+			pdf.push_back (NAN);
+			cdf.push_back (last);
+
+			size_t j = 0;
+			size_t size = values.size();
+			for (size_t i = 1; i < size; i++) {
+
+				// Get the range split value
+				const double x = values [i];
+
+				// Skip all the values that are less than the X value
+				while (raw_values [j] < x) ++j;
+
+				// Get the last CDF point where the skip condition was correct
+				const double less_x = raw_values [j - 1];
+				const double less_y = raw_cdf [j - 1];
+
+				// Calculate the gain value for the linear interpolation
+				const double gain = (x - less_x) / (raw_values [j] - less_x);
+
+				// Compute interpolated value of the CDF function at the target point
+				const double cur = (1.0 - gain) * less_y + gain * raw_cdf [j];
+
+				// Save computed PDF and CDF values
+				pdf.push_back (cur - last);
+				cdf.push_back (cur);
+
+				// Update the last computed CDF point
+				last = cur;
+			}
+		}
+	}
+
+	ContinuousDistribution (
+		const list &py_list,			// Empirical dataset
+		size_t bins						// Bins count for a histogram
+	) : ContinuousDistribution (to_vector (py_list), bins)
+	{}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructor for theoretical model                                     //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	ContinuousDistribution (
+		const BaseModel &model,			// Theoretical model of the CDF
+		const Range &range,				// Values range
+		size_t bins						// Bins count for a histogram
+	) : range (range)
+	{
+		// Split the range into sub-ranges (bins)
+		values = range.Split (bins);
+
+		// Fill the theoretical PDF and CDF tables
+		double last = NAN;
+		for (const auto x : values) {
+			const double cur = model.CDF (x);
+			pdf.push_back (cur - last);
+			cdf.push_back (cur);
+			last = cur;
+		}
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Domain of the CDF                                                     //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	const Range& Domain (void) const {
+		return range;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Count of stored CDF data points                                       //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	int Size (void) const {
+		return cdf.size();
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Bins count the CDF function has been split for a histogram            //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	int Bins (void) const {
+		return cdf.size() - 1;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Unique values in the dataset                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	const vector <double>& Values (void) const {
+		return values;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Values of the PDF function for the dataset                            //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	const vector <double>& PDF (void) const {
+		return pdf;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Smoothed values of the CDF function for the dataset                   //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	vector <double> SmoothedPDF (
+		size_t points
+	) const {
+		// TODO: Implement these functions
+//		const auto filter = filters.SmoothFilter (points);
+//		return filter.Apply (pdf);
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Values of the CDF function for the dataset                            //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	const vector <double>& CDF (void) const {
+		return cdf;
+	}
+};
+
+//****************************************************************************//
+//      Translate the object to a string                                      //
+//****************************************************************************//
+ostream& operator << (ostream &stream, const ContinuousDistribution &object)
+{
+	auto restore = stream.precision();
+	stream.precision (PRECISION);
+	stream << "\nCONTINUOUS DISTRIBUTION:" << std::endl;
+	stream << "========================" << std::endl;
+	stream << "Bins\t\t\t\t\t= " << object.Bins() << endl;
+	stream << object.Domain();
+	stream.precision (restore);
+	return stream;
+}
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
