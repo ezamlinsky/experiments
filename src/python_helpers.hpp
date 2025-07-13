225f7d2b686e4bd9c7c4a98168e7dd26a2910f23
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-07-13 18:41:32 +0300

    Add a new file 'python_helpers.hpp'

---
+++ src/python_helpers.hpp
@@ -0,0 +1,31 @@
+/*
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                  HELPER FUNCTIONS TO CONNECT C++ AND PYTHON                  #
+#                                                                              #
+# Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
+################################################################################
+*/
+# pragma	once
+# include	<vector>
+# include	<boost/python.hpp>
+
+// Use shortenings
+using namespace boost::python;
+using boost::python::list;
+
+//****************************************************************************//
+//      Convert a Python list to a standard array                             //
+//****************************************************************************//
+inline vector <double> to_vector (
+	const list &py_list				// The Python list to convert
+){
+	using lit = boost::python::stl_input_iterator <double>;
+	return vector <double> (lit (py_list), lit ());
+}
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
