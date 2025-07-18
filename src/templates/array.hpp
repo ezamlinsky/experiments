51bdb87eb6faba51c214e335963f3879518c42ac
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-07-18 17:16:21 +0300

    Add a new file 'array.hpp'

---
+++ src/templates/array.hpp
@@ -0,0 +1,24 @@
+/*
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                               ARRAY FUNCTIONS                                #
+#                                                                              #
+# Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
+################################################################################
+*/
+# pragma	once
+# include	<cmath>
+# include	<algorithm>
+# include	"array/unary.hpp"
+# include	"array/binary.hpp"
+# include	"array/min_max.hpp"
+# include	"array/bin_search.hpp"
+# include	"array/sum.hpp"
+# include	"array/median.hpp"
+
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
