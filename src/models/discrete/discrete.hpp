830503c074e85b657ad0321f8c9edb2cd14e27f7
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-07-23 20:35:49 +0300

    Add a new base class 'BaseDiscrete' for discrete distributions

---
+++ src/models/discrete/discrete.hpp
@@ -0,0 +1,78 @@
+/*
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                 BASE CLASS FOR DISCRETE DISTRIBUTION MODELS                  #
+#                                                                              #
+# Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
+################################################################################
+*/
+# pragma	once
+# include	"../base.hpp"
+
+//****************************************************************************//
+//      Class "BaseDiscrete"                                                  //
+//****************************************************************************//
+class BaseDiscrete : public BaseModel
+{
+//============================================================================//
+//      Private methods                                                       //
+//============================================================================//
+private:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Estimation of the distribution quartile                               //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Quartile (
+		double level			// Quantile level to estimate
+	) const override final {
+
+		// Adjust the raw value of the target quantile when required
+		const double value = Quantile (level);
+		return CDF (value) == level ? value + 0.5 : value;
+	}
+
+//============================================================================//
+//      Public methods                                                        //
+//============================================================================//
+public:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructor                                                           //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	BaseDiscrete (void) = default;
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Quantile value for the target level                                   //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Quantile (
+		double level			// Quantile level to estimate
+	) const override final {
+
+		// Check if the level is correct
+		if (0.0 <= level and level <= 1.0) {
+
+			// Get the distribution domain to clamp the argument value
+			const Range &range = Domain();
+
+			// Binary search of argument value for the CDF function
+			int64_t left = range.Min();
+			int64_t right = range.Max();
+			while (left < right) {
+				const int64_t x = (left + right) / 2;
+				if (CDF (x) < level)
+					left = x + 1;
+				else
+					right = x;
+			}
+			return left;
+		}
+		else
+			throw invalid_argument ("Quantile: Level must be in the range [0..1]");
+	}
+};
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
