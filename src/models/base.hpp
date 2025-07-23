13dea594afefd81fd45d04e59209cdc90c6f545a
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-07-23 20:27:20 +0300

    Split the class 'BaseModel'

--- src/models/base.hpp
+++ src/models/base.hpp
@@ -14,12 +14,6 @@
 # include	"range.hpp"
 # include	"../observations/observations.hpp"
 
-// Value of the step out of a point where the derivative is infinite
-# define	STEP_OUTSIDE		0.001
-
-// The number of iterations for the Newton solve method
-# define	NEWTON_ITERATIONS	8
-
 //****************************************************************************//
 //      Class "BaseModel"                                                     //
 //****************************************************************************//
@@ -69,76 +63,24 @@ public:
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-//      Quantile value for the target level                                   //
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-	double Quantile (
-		double level			// Quantile level to estimate
-	) const {
-
-		// Check if the level is correct
-		if (0.0 <= level and level <= 1.0) {
-
-			// Get the distribution domain to clamp the argument value
-			const Range &range = Domain();
-
-			// The first approximation is the mode value (if it exists),
-			// then the mean value
-			double x = Mode();
-			if (isnan (x)) x = Mean();
-			if (isinf (x)) x = 0.0;
-
-			// Find a solution using the Newton solve method
-			int i = NEWTON_ITERATIONS;
-			while (i--) {
-
-				// Calculate the function and its derivative
-				const double func = CDF (x);
-				const double der = PDF (x);
-
-				// Check the distance between the function and the target value
-				const double diff = func - level;
-
-				// Calculate a step value to move for the next point
-				double step = diff / der;
-
-				// If the derivative is infinite in the target point
-				// then do a step outside the pit
-				if (der == INFINITY)
-					step = +STEP_OUTSIDE * diff;
-				else if (der == -INFINITY)
-					step = -STEP_OUTSIDE * diff;
-
-				// A new approximation
-				x -= step;
-
-				// Clamp the value inside the distribution domain
-				x = range.Clamp (x);
-			}
-			return x;
-		}
-		else
-			throw invalid_argument ("Quantile: Level must be in the range [0..1]");
-	}
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 //      Median of the distribution                                            //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	double Median (void) const {
-		return Quantile (0.5);
+		return Quartile (0.5);
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 //      Lower quartile of the distribution                                    //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	double LowerQuartile (void) const {
-		return Quantile (0.25);
+		return Quartile (0.25);
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 //      Upper quartile of the distribution                                    //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	double UpperQuartile (void) const {
-		return Quantile (0.75);
+		return Quartile (0.75);
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
@@ -193,6 +135,8 @@ public:
 //      Virtual functions to override in derivative classes                   //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	virtual const Range& Domain (void) const = 0;
+	virtual double Quartile (double level) const = 0;
+	virtual double Quantile (double level) const = 0;
 	virtual double PDF (double x) const = 0;
 	virtual double CDF (double x) const = 0;
 	virtual double Mode (void) const = 0;
