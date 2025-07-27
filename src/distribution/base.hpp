33b8f331cdb4a1716bb57c40a67d7cc6044331ae
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-07-28 01:08:20 +0300

    Add a new file 'base.hpp'

---
+++ src/distribution/base.hpp
@@ -0,0 +1,116 @@
+/*
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#         CONTAIN A SAMPLE AND A REFERENCE DISTRIBUTION FOR COMPARISON         #
+#                                                                              #
+# Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
+################################################################################
+*/
+# pragma	once
+# include	<cmath>
+# include	"distribution.hpp"
+
+//****************************************************************************//
+//      Class "BaseComparator"                                                //
+//****************************************************************************//
+class BaseComparator
+{
+//============================================================================//
+//      Members                                                               //
+//============================================================================//
+protected:
+	Distribution sample;					// Sample distribution to compare
+	Distribution reference;					// Reference distribution
+
+//============================================================================//
+//      Protected methods                                                     //
+//============================================================================//
+protected:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructors                                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+	// Discrete distribution
+	BaseComparator (
+		const Observations &data			// Observations of a random value
+	) : sample (data)
+	{}
+
+	// Discrete distribution
+	BaseComparator (
+		const vector <double> &data			// Empirical data
+	) : sample (data)
+	{}
+
+	// Continuous distribution
+	BaseComparator (
+		const Observations &data,			// Observations of a random value
+		size_t bins							// Bins count for a histogram
+	) : sample (data, bins)
+	{}
+
+	// Continuous distribution
+	BaseComparator (
+		const vector <double> &data,		// Empirical data
+		size_t bins							// Bins count for a histogram
+	) : sample (data, bins)
+	{}
+
+//============================================================================//
+//      Public methods                                                        //
+//============================================================================//
+public:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Load a distribution model as a reference for the distribution test    //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	void ReferenceModel (
+		const BaseModel &model				// Theoretical model
+	){
+		// Check if empirical data range is inside the model domain
+		const Range &range =  model.Domain();
+		if (range.IsInside (sample.Domain())) {
+
+			// Set the distribution model
+			reference = Distribution (model, sample.Values());
+		}
+		else
+			throw invalid_argument ("ReferenceModel: The sample data range is outside the distribution model domain");
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Return sample distribution                                            //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	const Distribution& Sample (void) const {
+		return sample;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Return reference distribution                                         //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	const Distribution& Reference (void) const {
+		return reference;
+	}
+};
+
+//****************************************************************************//
+//      Translate the object to a string                                      //
+//****************************************************************************//
+ostream& operator << (ostream &stream, const BaseComparator &object)
+{
+	auto restore = stream.precision();
+	stream.precision (PRECISION);
+	stream << "\nDISTRIBUTION COMPARATOR:" << endl;
+	stream << "========================" << endl;
+	stream << "Sample data points\t\t\t= " << object.Sample().Bins() << endl;
+	stream << "Reference data points\t\t\t= " << object.Reference().Bins() << endl;
+	stream.precision (restore);
+	return stream;
+}
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
