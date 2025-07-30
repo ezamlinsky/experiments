1a7225092beb65c4d50ba4d9e299404b02cb71ff
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-07-30 20:43:42 +0300

    Add a new file 'bernoulli.hpp'

---
+++ src/models/discrete/bernoulli.hpp
@@ -0,0 +1,199 @@
+/*
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                DISTRIBUTION MODEL FOR BERNOULLI DISTRIBUTION                 #
+#                                                                              #
+# Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
+################################################################################
+*/
+# pragma	once
+# include	"discrete.hpp"
+
+//****************************************************************************//
+//      Class "Bernoulli"                                                     //
+//****************************************************************************//
+class Bernoulli final : public BaseDiscrete
+{
+//============================================================================//
+//      Members                                                               //
+//============================================================================//
+private:
+	static const Range range;		// Function domain where the distribution exists
+	static const size_t params;		// Count of distribution parameters
+	const double probability;		// Probability of a successful trial
+
+// Extract the distribution parameters from empirical observations
+struct Params {
+
+	// Members
+	double probability;				// The first shape parameter
+
+	// Constructor
+	Params (
+		const Observations &data	// Empirical observations
+	){
+		// Check if empirical data range is inside the model domain
+		if (Bernoulli::InDomain (data.Domain())) {
+
+			// Extract parameters from the empirical observations
+			probability = data.Mean();
+		}
+		else
+			throw invalid_argument ("Bernoulli: The data range is outside the distribution domain");
+	}
+};
+
+//============================================================================//
+//      Private methods                                                       //
+//============================================================================//
+private:
+	Bernoulli (
+		const Params &params		// Distribution parameters
+	) : Bernoulli (params.probability)
+	{}
+
+//============================================================================//
+//      Public methods                                                        //
+//============================================================================//
+public:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructor                                                           //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	Bernoulli (
+		double probability			// Probability of a successful trial
+	) : probability (probability)
+	{
+		if (probability < 0)
+			throw invalid_argument ("Bernoulli: The probability of a successful trial must be positive");
+		if (probability > 1.0)
+			throw invalid_argument ("Bernoulli: The probability of a successful trial can not be greater than 1");
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructor for empirical data                                        //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	Bernoulli (
+		const Observations &data	// Empirical observations
+	) : Bernoulli (Params (data))
+	{}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Check if the range is inside the model domain                         //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	static bool InDomain (
+		const Range &subrange		// Testing range
+	){
+		return range >= subrange;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Probability of a successful trial                                     //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	double Probability (void) const {
+		return probability;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Function domain where the distribution exists                         //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual const Range& Domain (void) const override final {
+		return range;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Number of distribution parameters to describe the population          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual size_t Parameters (void) const override final {
+		return params;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Probability Density Function (PDF)                                    //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double PDF (
+		double x					// Argument value
+	) const override final {
+
+		// Outside the distribution domain
+		if (x != range) return 0.0;
+
+		// Common case
+		return 1.0 - probability;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Cumulative Distribution Function (CDF)                                //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double CDF (
+		double x					// Argument value
+	) const override final {
+
+		// Below the range
+		if (x < range) return 0.0;
+
+		// Above the range
+		if (x >= range) return 1.0;
+
+		// Common case
+		return 1.0 - probability;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Mode of the distribution                                              //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Mode (void) const override final {
+		if (probability < 0.5) return 0.0;
+		if (probability > 0.5) return 1.0;
+		return NAN;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Mean of the distribution                                              //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Mean (void) const override final {
+		return probability;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Variance of the distribution                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Variance (void) const override final {
+		return probability * (1.0 - probability);
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Clone the distribution model                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual unique_ptr <const BaseModel> clone (void) const override final {
+		return unique_ptr <const BaseModel> (new Bernoulli (*this));
+	}
+};
+
+//****************************************************************************//
+//      Internal constants used by the class                                  //
+//****************************************************************************//
+const Range Bernoulli::range = Range (0.0, 1.0);
+const size_t Bernoulli::params = 1;
+
+//****************************************************************************//
+//      Translate the object to a string                                      //
+//****************************************************************************//
+ostream& operator << (ostream &stream, const Bernoulli &model)
+{
+	auto restore = stream.precision();
+	stream.precision (PRECISION);
+	stream << "\nBERNOULLI DISTRIBUTION:" << std::endl;
+	stream << "=======================" << std::endl;
+	stream << static_cast <const BaseDiscrete&> (model);
+	stream << "    Success probability\t\t\t= " << model.Probability() << endl;
+	stream << static_cast <const BaseModel&> (model);
+	stream.precision (restore);
+	return stream;
+}
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
