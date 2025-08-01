a9cbbd7e9827382c86c6aeddc1b3135476418f04
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-08-01 21:44:47 +0300

    Add a new file 'geometric.hpp'

---
+++ src/models/discrete/geometric.hpp
@@ -0,0 +1,207 @@
+/*
+################################################################################
+# Encoding: UTF-8                                                  Tab size: 4 #
+#                                                                              #
+#                DISTRIBUTION MODEL FOR GEOMETRIC DISTRIBUTION                 #
+#                                                                              #
+# Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
+################################################################################
+*/
+# pragma	once
+# include	"discrete.hpp"
+
+//****************************************************************************//
+//      Class "Geometric"                                                     //
+//****************************************************************************//
+class Geometric final : public BaseDiscrete
+{
+//============================================================================//
+//      Members                                                               //
+//============================================================================//
+private:
+	static const Range range;		// Function domain where the distribution exists
+	static const size_t params;		// Count of distribution parameters
+	const double probability;		// Probability of a successful trial
+
+// Extract the distribution parameters from empirical observations
+struct Params {
+
+	// Members
+	double probability;				// The first shape parameter
+
+	// Constructor
+	Params (
+		const Observations &data	// Empirical observations
+	){
+		// Check if empirical data range is inside the model domain
+		if (Geometric::InDomain (data.Domain())) {
+
+			// Extract parameters from the empirical observations
+			probability = 1.0 / (1.0 + data.Mean());
+			if (probability < 0.0 || probability > 1.0)
+				throw invalid_argument ("Geometric: Can not estimate the probability value for the dataset");
+		}
+		else throw invalid_argument ("Geometric: The data range is outside the distribution domain");
+	}
+};
+
+//============================================================================//
+//      Private methods                                                       //
+//============================================================================//
+private:
+	Geometric (
+		const Params &params		// Distribution parameters
+	) : Geometric (params.probability)
+	{}
+
+//============================================================================//
+//      Public methods                                                        //
+//============================================================================//
+public:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructor                                                           //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	Geometric (
+		double probability			// Probability of a successful trial
+	) : probability (probability)
+	{
+		if (probability < 0.0 || probability > 1.0)
+			throw invalid_argument ("Geometric: The probability of a successful trial must be in the range [0..1]");
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Constructor for empirical data                                        //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	Geometric (
+		const Observations &data	// Empirical observations
+	) : Geometric (Params (data))
+	{}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Check if the range is inside the model domain                         //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	static bool InDomain (
+		const Range &subrange		// Testing range
+	){
+		return range >= subrange;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Probability of a successful trial                                     //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	double Probability (void) const {
+		return probability;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Function domain where the distribution exists                         //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual const Range& Domain (void) const override final {
+		return range;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Number of distribution parameters to describe the population          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual size_t Parameters (void) const override final {
+		return params;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Probability Density Function (PDF)                                    //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double PDF (
+		double x					// Argument value
+	) const override final {
+
+		// Outside the distribution domain
+		if (x < range) return 0.0;
+
+		// Common case
+		const size_t arg = floor (x);
+		return probability * pow (1.0 - probability, arg);
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Cumulative Distribution Function (CDF)                                //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double CDF (
+		double x					// Argument value
+	) const override final {
+
+		// Below the range
+		if (x < range) return 0.0;
+
+		// Common case
+		const size_t arg = floor (x);
+		return 1.0 - pow (1.0 - probability, arg + 1.0);
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Mode of the distribution                                              //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Mode (void) const override final {
+		return 0.0;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Mean of the distribution                                              //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Mean (void) const override final {
+		return (1.0 - probability) / probability;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Variance of the distribution                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Variance (void) const override final {
+		return (1.0 - probability) / (probability * probability);
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Skewness of the distribution                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Skewness (void) const override final {
+		const double p = 2.0 - probability;
+		const double q = sqrt (1.0 - probability);
+		return p / q;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Kurtosis of the distribution                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Kurtosis (void) const override final {
+		const double temp = probability;
+		const double p = temp * temp;
+		const double q = 1.0 - probability;
+		return 6.0 + p / q;
+	}
+};
+
+//****************************************************************************//
+//      Internal constants used by the class                                  //
+//****************************************************************************//
+const Range Geometric::range = Range (0.0, INFINITY);
+const size_t Geometric::params = 1;
+
+//****************************************************************************//
+//      Translate the object to a string                                      //
+//****************************************************************************//
+ostream& operator << (ostream &stream, const Geometric &model)
+{
+	auto restore = stream.precision();
+	stream.precision (PRECISION);
+	stream << "\nGEOMETRIC DISTRIBUTION:" << std::endl;
+	stream << "=======================" << std::endl;
+	stream << static_cast <const BaseDiscrete&> (model);
+	stream << "    Success probability\t\t\t= " << model.Probability() << endl;
+	stream << static_cast <const BaseModel&> (model);
+	stream.precision (restore);
+	return stream;
+}
+/*
+################################################################################
+#                                 END OF FILE                                  #
+################################################################################
+*/
