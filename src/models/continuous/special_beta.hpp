5f894e5e62301c7ac1cbf04bd185954979bfa480
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-08-07 06:26:36 +0300

    Replace the continuous fraction used to calculate  the incomplete Beta function

--- src/models/continuous/special_beta.hpp
+++ src/models/continuous/special_beta.hpp
@@ -12,58 +12,7 @@
 # include	<stdexcept>
 
 // Number of iterations to calculate the continued fraction
-# define	BETA_N		26
-
-//****************************************************************************//
-//      The helper function to calculate the trimmed continued fraction       //
-//****************************************************************************//
-// Arguments are always correct because of preceding checks in the caller class
-static void continued_fraction (
-	double &p,		// Where to return the P value of continued fraction
-	double &q,		// Where to return the Q value of continued fraction
-	double x,		// Argument value
-	double a,		// A argument of the incomplete beta function
-	double b		// B argument of the incomplete beta function
-){
-	// Internal variable are immutable in the loop
-	const double ab = a + b;
-	const double ap = a + 1.0;
-	const double am = a - 1.0;
-
-	// Intermediate P and Q for the loop
-	double p1 = 1.0;
-	double q1 = 1.0;
-
-	// Calculate the trimmed continued fraction
-	int j = BETA_N;
-	while (j) {
-
-		// We do it from the tail
-		const int j2 = 2 * j;
-
-		// Odd term
-		const double odd1 = -(a + j) * (ab + j) * x;
-		const double odd2 = (a + j2) * (ap + j2);
-		const double odd = odd1 / odd2;
-		const double p2 = p1 + odd * q1;
-		const double q2 = p1;
-
-		// Even term
-		const double even1 = j * (b - j) * x;
-		const double even2 = (am + j2) * (a + j2);
-		const double even = even1 / even2;
-		p1 = p2 + even * q2;
-		q1 = p2;
-
-		// Decrement the counter
-		--j;
-	}
-
-	// Finalize the calculation of the continued fraction
-	const double odd = ab * x / ap;
-	p = p1 - odd * q1;
-	q = p1;
-}
+# define	BETA_N		52
 
 //****************************************************************************//
 //      Special beta function                                                 //
@@ -81,19 +30,54 @@ private:
 //============================================================================//
 //      Private methods                                                       //
 //============================================================================//
-	double core (
-		double x,	// Argument value
-		double a,	// A argument of the incomplete beta function
-		double b	// B argument of the incomplete beta function
-	) const {
 
-		// Evaluate the continued fraction part
-		double p, q;
-		continued_fraction (p, q, x, a, b);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      The helper function to calculate the trimmed continued fraction       //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	// Arguments are always correct because of preceding checks in the caller class
+	static double continued_fraction (
+		double x,			// Argument value
+		double a,			// A argument of the incomplete beta function
+		double b			// B argument of the incomplete beta function
+	){
+		double res = 0.0;
+		for (size_t i = BETA_N; i > 0; i--) {
+			const double temp1 = a + 2.0 * i;
+			const double temp2 = temp1 - 1.0;
+			const double temp3 = temp1 + 1.0;
+			const double temp4 = a + i;
+			const double temp5 = b - i;
+			const double temp6 = x / temp2;
 
-		// Combine the parts to get the result
+			// Intermediate P and Q for the loop
+			const double p = (temp4 - 1.0) * (temp4 + b - 1.0) * temp5 * i * temp6 * temp6;
+			const double q = temp1 + (i * temp5 / temp2 - temp4 * (temp4 + b) / temp3) * x;
+
+			// Update the intermediate result
+			res = p / (q + res);
+		}
+
+		// Finalize the calculation of the continued fraction
+		const double p = 1.0;
+		const double q = a * (1.0 - (a + b) / (a + 1.0) * x) + res;
+		return p / q;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Incomplete beta function                                              //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	static double incomplete (
+		double x,			// Argument value
+		double a,			// A argument of the incomplete beta function
+		double b,			// B argument of the incomplete beta function
+		double denominator	// The denominator value for the incomplete beta function
+	){
 		const double temp = a * log (x) + b * log (1.0 - x);
-		return exp (temp - beta_log) * q / (p * a);
+		const double scale = exp (temp - denominator);
+		if (x <= a / (a + b))
+			return continued_fraction (x, a, b) * scale;
+		else
+			return 1.0 - continued_fraction (1.0 - x, b, a) * scale;
 	}
 
 //============================================================================//
@@ -105,8 +89,8 @@ public:
 //      Constructor                                                           //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	SpecialBeta (
-		double a,	// A argument of the beta function
-		double b	// B argument of the beta function
+		double a,			// A argument of the beta function
+		double b			// B argument of the beta function
 	) : beta_a (a),
 		beta_b (b),
 		beta_log (lgamma (a) + lgamma (b) - lgamma (a + b))
@@ -118,10 +102,10 @@ public:
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-//      Regularized incomplete beta function                                  //
+//      Incomplete beta function                                              //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-	double RegIncompleteBeta (
-		double x	// Argument value
+	double IncompleteBeta (
+		double x			// Argument value
 	) const {
 
 		// Check the argument range
@@ -131,17 +115,37 @@ public:
 			if (x == 0.0) return 0.0;
 			if (x == 1.0) return 1.0;
 
-			// The threshold value
-			const double threshold = (beta_a + 1.0) / (beta_a + beta_b + 2.0);
-
-			// Return the result or its complement based on the initial check
-			if (x > threshold)
-				return 1.0 - core (1.0 - x, beta_b, beta_a);
-			else
-				return core (x, beta_a, beta_b);
+			// Common case
+			return incomplete (x, beta_a, beta_b, 0.0);
 		}
-		else
-			throw invalid_argument ("RegIncompleteBeta: The argument must be in the range [0..1]");
+		else throw invalid_argument ("IncompleteBeta: The argument must be in the range [0..1]");
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Regularized incomplete beta function                                  //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	double RegIncompleteBeta (
+		double x			// Argument value
+	) const {
+
+		// Check the argument range
+		if (0.0 <= x && x <= 1.0) {
+
+			// Special cases
+			if (x == 0.0) return 0.0;
+			if (x == 1.0) return 1.0;
+
+			// Common case
+			return incomplete (x, beta_a, beta_b, beta_log);
+		}
+		else throw invalid_argument ("RegIncompleteBeta: The argument must be in the range [0..1]");
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Beta function                                                         //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	double Beta (void) const {
+		return exp (beta_log);
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
