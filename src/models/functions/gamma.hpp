f2362656f17dcf0a5babb1a2fc2170ac125d0984
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-08-09 00:59:40 +0300

    Add normalized incomplete gamma functions for a half argument

--- src/models/functions/gamma.hpp
+++ src/models/functions/gamma.hpp
@@ -11,15 +11,17 @@
 # include	<cmath>
 
 //****************************************************************************//
-//      Normalized upper incomplete gamma function                            //
+//      Normalized upper incomplete gamma function for an integer argument    //
 //****************************************************************************//
 double NormalizedUpperIncompleteGamma (
 	double arg,					// Argument value
 	size_t count				// Iterations count
 ){
+	// Check if the argument value is correct
 	if (arg <= 0.0)
 		throw invalid_argument ("NormalizedUpperIncompleteGamma: The argument value must be positive");
 
+	// Compute the finite sum
 	const double arg_log = log (arg);
 	double sum = 0.0;
 	for (size_t i = 0; i < count; i++) {
@@ -28,14 +30,46 @@ double NormalizedUpperIncompleteGamma (
 	}
 	return sum;
 }
+
 //****************************************************************************//
-//      Normalized lower incomplete gamma function                            //
+//      Normalized upper incomplete gamma function for a half argument        //
+//****************************************************************************//
+double NormalizedUpperIncompleteGammaHalf (
+	double arg,					// Argument value
+	size_t count				// Iterations count
+){
+	// Check if the argument value is correct
+	if (arg <= 0.0)
+		throw invalid_argument ("NormalizedUpperIncompleteGammaHalf: The argument value must be positive");
+
+	// Compute the finite sum
+	const double arg_log = log (arg);
+	double sum = 0.0;
+	for (size_t i = 0; i < count; i++) {
+		const double temp = (i + 0.5) * arg_log - arg - lgamma (i + 1.5);
+		sum += exp (temp);
+	}
+	return erfc (sqrt (arg)) + sum;
+}
+
+//****************************************************************************//
+//      Normalized lower incomplete gamma function for an integer argument    //
 //****************************************************************************//
 double NormalizedLowerIncompleteGamma (
 	double arg,					// Argument value
 	size_t count				// Iterations count
 ){
 	return 1.0 - NormalizedUpperIncompleteGamma (arg, count);
+}
+
+//****************************************************************************//
+//      Normalized lower incomplete gamma function for a half argument        //
+//****************************************************************************//
+double NormalizedLowerIncompleteGammaHalf (
+	double arg,					// Argument value
+	size_t count				// Iterations count
+){
+	return 1.0 - NormalizedUpperIncompleteGammaHalf (arg, count);
 }
 /*
 ################################################################################
