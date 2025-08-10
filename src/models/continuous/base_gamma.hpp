70615fe08a9cb9e545bd1880ecd805d3ed667f90
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-08-10 19:01:11 +0300

    Rename the file 'special_gamma.hpp' to 'base_gamma.hpp'

--- src/models/continuous/base_gamma.hpp
+++ src/models/continuous/base_gamma.hpp
@@ -14,18 +14,17 @@
 //****************************************************************************//
 //      Special class for PDF and CDF for gamma-related distributions         //
 //****************************************************************************//
-class SpecialGamma : public BaseContinuous
+class BaseGamma : public BaseContinuous
 {
 //============================================================================//
 //      Members                                                               //
 //============================================================================//
 private:
 	static const Range range;	// Function domain where the distribution exists
-	const double gamma_log;		// Logarithm of the gamma function
-
-protected:
-	const size_t gamma_shape;	// Shape of the gamma distribution
-	const double gamma_scale;	// Scale of the gamma distribution
+	const size_t shape;			// Shape of the derivative distribution
+	const size_t gamma_shape;	// Shape of the base gamma distribution
+	const double gamma_scale;	// Scale of the base gamma distribution
+	const double gamma_log;		// Logarithm of the base gamma function
 
 //============================================================================//
 //      Public methods                                                        //
@@ -33,19 +32,34 @@ protected:
 public:
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-//      Constructor                                                           //
+//      Constructors                                                          //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-	SpecialGamma (
-		size_t shape,			// Shape of the gamma distribution
-		double scale			// Scale of the gamma distribution
-	) : gamma_log (lgamma (0.5 * shape)),
-		gamma_shape (shape),
-		gamma_scale (scale)
+
+	// Erlang distribution
+	BaseGamma (
+		size_t shape,			// Shape of the derivative distribution
+		double scale			// Scale of the derivative distribution
+	) :	shape (shape),
+		gamma_shape (2 * shape),
+		gamma_scale (scale),
+		gamma_log (lgamma (shape))
 	{
 		if (shape == 0.0)
-			throw invalid_argument ("SpecialGamma: The shape value must be positive");
+			throw invalid_argument ("BaseGamma: The shape value must be positive");
 		if (scale <= 0.0)
-			throw invalid_argument ("SpecialGamma: The scale value must be positive");
+			throw invalid_argument ("BaseGamma: The scale value must be positive");
+	}
+
+	// Chi-squared distribution
+	BaseGamma (
+		size_t shape			// Shape of the derivative distribution
+	) :	shape (shape),
+		gamma_shape (shape),
+		gamma_scale (2.0),
+		gamma_log (lgamma (0.5 * shape))
+	{
+		if (shape == 0.0)
+			throw invalid_argument ("BaseGamma: The shape value must be positive");
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
@@ -55,6 +69,20 @@ public:
 		const Range &subrange	// Testing range
 	){
 		return range >= subrange;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Shape of the distribution                                             //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	size_t Shape (void) const {
+		return shape;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Scale of the distribution                                             //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	double Scale (void) const {
+		return gamma_scale;
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
@@ -105,12 +133,50 @@ public:
 		else
 			return NormalizedLowerIncompleteGamma (arg, count);
 	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Mode of the distribution                                              //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Mode (void) const override final {
+		if (gamma_shape >= 2)
+			return 0.5 * gamma_scale * (gamma_shape - 2);
+		else
+			return 0;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Mean of the distribution                                              //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Mean (void) const override final {
+		return 0.5 * gamma_scale * gamma_shape;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Variance of the distribution                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Variance (void) const override final {
+		return 0.5 * gamma_scale * gamma_scale * gamma_shape;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Skewness of the distribution                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Skewness (void) const override final {
+		return 2.0 * sqrt (2.0 / gamma_shape);
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Kurtosis of the distribution                                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	virtual double Kurtosis (void) const override final {
+		return 3.0 + 12.0 / gamma_shape;
+	}
 };
 
 //****************************************************************************//
 //      Internal constants used by the class                                  //
 //****************************************************************************//
-const Range SpecialGamma::range = Range (0.0, INFINITY);
+const Range BaseGamma::range = Range (0.0, INFINITY);
 /*
 ################################################################################
 #                                 END OF FILE                                  #
