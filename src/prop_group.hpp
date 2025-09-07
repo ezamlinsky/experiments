ca6a52bec0e7b212054d910bc47d51e56b403fb0
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-09-07 14:50:21 +0300

    Improve the class 'PropGroup'

--- src/prop_group.hpp
+++ src/prop_group.hpp
@@ -9,11 +9,11 @@
 */
 # pragma	once
 # include	<iostream>
-# include	<vector>
+# include	<deque>
 
 // Use shortenings
 using namespace std;
-using properties = vector <pair <string, double>>;
+using properties = deque <pair <string, double>>;
 
 //****************************************************************************//
 //      Class "PropGroup"                                                     //
@@ -24,8 +24,8 @@ class PropGroup
 //      Members                                                               //
 //============================================================================//
 private:
-	const string name;				// Name of the properties group
-	properties props;				// List of the properties
+	string group_name;			// Name of the properties group
+	properties props;			// List of the properties
 
 //============================================================================//
 //      Public methods                                                        //
@@ -36,26 +36,46 @@ public:
 //      Constructor                                                           //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	PropGroup (
-		const string &name			// Name of the properties group
-	) :	name (name)
+		const string &name		// Name of the properties group
+	) :	group_name (name)
 	{}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Prepend a property to the list                                        //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	void Prepend (
+		const string &property,	// Object property name
+		double value			// Value of the object property
+	){
+		// Append the property to the list
+		props.push_front (pair <string, double> (property, value));
+	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 //      Append a property to the list                                         //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	void Append (
-		const string &property,		// Object property name
-		double value				// Value of the object property
+		const string &property,	// Object property name
+		double value			// Value of the object property
 	){
 		// Append the property to the list
 		props.push_back (pair <string, double> (property, value));
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-//      Name of the properties group                                          //
+//      Set name of the properties group                                      //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	void Name (
+		const string &name		// New name of the properties group
+	){
+		group_name = name;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Get name of the properties group                                      //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 	const string& Name (void) const {
-		return name;
+		return group_name;
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
@@ -64,7 +84,11 @@ public:
 	const properties& Properties (void) const {
 		return props;
 	}
+	properties& Properties (void) {
+		return props;
+	}
 };
+using groups = deque <PropGroup>;
 
 //****************************************************************************//
 //      Translate the object to a string                                      //
