d6b57681dbb551403050bb5304f15eb2e965da70
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-11-07 20:16:07 +0200

    Refactor the source code of the 'MinDelayFilter' class

--- src/filters/min_delay.hpp
+++ src/filters/min_delay.hpp
@@ -7,6 +7,7 @@
 # Ordnung muss sein!                             Copyleft (Ɔ) Eugene Zamlinsky #
 ################################################################################
 */
+# pragma	once
 # include	"smooth.hpp"
 
 //****************************************************************************//
@@ -14,6 +15,62 @@
 //****************************************************************************//
 class MinDelayFilter : public Filter
 {
+//============================================================================//
+//      Private methods                                                       //
+//============================================================================//
+private:
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Compute values of the windowed Sinc function                          //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	static vector <double> SincFilter (
+		size_t points				// Count of neighbor points to smooth by
+	){
+		// Filter period and size
+		const size_t period = points + 1;
+		const size_t size = (4 * EXTRA_PERIOD * period - points) / 2;
+
+		// Blackman window properties
+		const double alpha = 0.16;
+		const double a0 = 0.5 * (1.0 - alpha);
+		const double a1 = 0.5;
+		const double a2 = 0.5 * alpha;
+
+		// Compute the impulse response function
+		vector <double> impulse;
+		impulse.push_back (1.0);
+		for (size_t i = 1; i < size; i++) {
+
+			// Sinc function
+			const double x = M_PI * i / period;
+			const double sinc = sin (x) / x;
+
+			// Blackman window
+			const double temp = M_PI * i / size;
+			const double win = a0 + a1 * cos (temp) + a2 * cos (2 * temp);
+
+			// The impulse response with applied window function
+			impulse.push_back (sinc * win);
+		}
+
+		// Return the impulse response
+		return impulse;
+	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//      Normalize the filter impulse response                                 //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+	static void Normalize (
+		double impulse[],			// Filter impulse response
+		size_t size					// Size of impulse response
+	){
+		// Find the sum of the filter coefficients
+		const double sum = Array::Sum (impulse, size);
+
+		// Normalize all the filter coefficients to make the total sum equal to 1.0
+		Array::Div (impulse, size, sum);
+	}
+
 //============================================================================//
 //      Public methods                                                        //
 //============================================================================//
@@ -26,11 +83,8 @@ public:
 		size_t points				// Count of neighbor points to filter by
 	) :	Filter (points)
 	{
-		if (points == 0)
-			throw invalid_argument ("MinDelayFilter: The number of neighbor points must be positive");
-
 		// Calculate windowed sinc function
-		vector <double> sinc_half = sinc_filter (points);
+		vector <double> sinc_half = SincFilter (points);
 
 		// Reserve space for the full impulse response function
 		const size_t size = sinc_half.size();
@@ -46,21 +100,27 @@ public:
 		for (size_t i = 0; i < size; i++)
 			buffer [i + points] = sinc_half [i];
 
+		// Normalize the filter impulse response
+		Normalize (buffer.data(), buffer.size());
+
+		// Reverse the impulse response function
+		reverse (buffer.begin(), buffer.end());
+
 		// Set the impulse response
-		impulse = normalize (buffer);
+		impulse = buffer;
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 //      Apply the filter to the target time series                            //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-	vector <double> Apply (
+	virtual vector <double> Apply (
 		const vector <double> &data	// The time series to filter
-	) const {
+	) const override final {
 
 		// Clear the dataset from NaNs
 		DataCleaner cleaner = DataCleaner (data);
 
-		// Expand the clean dataset to the left and right
+		// Expand the clean dataset to the left
 		const size_t impulse_size = impulse.size();
 		const vector <double> &clean = cleaner.ExpandedData (impulse_size - 1, 0);
 
@@ -72,25 +132,12 @@ public:
 		for (size_t i = 0; i < size; i++) {
 
 			// Find the filter response for the moving window
-			double sum = 0.0;
-			for (size_t j = 0; j < impulse_size; j++) {
-				const size_t index = impulse_size - 1 - j;
-				sum += clean [i + index] * impulse [j];
-			}
-			response.push_back (sum);
+			const double conv = Array::SumMul (clean.data() + i, impulse.data(), impulse_size);
+			response.push_back (conv);
 		}
 
 		// Restore NaN values in the impulse response
 		return cleaner.Restore_NaNs (response);
-	}
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-//      Apply the filter to the target python list                            //
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-	vector <double> Apply (
-		const pylist &py_list		// The python list to filter
-	) const {
-		return Apply (to_vector (py_list));
 	}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
