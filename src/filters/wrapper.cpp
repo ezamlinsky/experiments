1ce4a813d0f224ebe8a4eb2a139a9fceab4ee9b2
Eugene Zamlinsky <ezamlinsky@gmail.com>
2025-11-08 00:12:01 +0200

    Expose new classes to Python

--- src/filters/wrapper.cpp
+++ src/filters/wrapper.cpp
@@ -9,21 +9,41 @@
 */
 # include	<boost/python.hpp>
 # include	"median.hpp"
-# include	"smooth.hpp"
+# include	"smooth_filter.hpp"
+# include	"diff_filter.hpp"
+# include	"swing_filter.hpp"
+# include	"hilbert_filter.hpp"
 # include	"min_delay.hpp"
 
 //****************************************************************************//
-//      Common methods for time series filters                                //
+//      Pointers to the filter overloaded methods                             //
 //****************************************************************************//
-# define	FILTERS_COMMON(class) 												\
+# define	POINTERS_COMMON(class) 												\
+vector <double> (class::*class##Apply)(const vector <double> &data) const	= &class::Apply;
+vector <double> (BaseFilter::*ListApply)(const pylist &py_list) const 		= &BaseFilter::Apply;
+
+//****************************************************************************//
+//      Base methods of the time series filters                               //
+//****************************************************************************//
+# define	FILTERS_BASE(class) 												\
 	.add_property ("Points",	&class::Points, 								\
 		"Count of neighbor points to filter by") 								\
+	.def ("Apply",				ListApply,		args ("data"),					\
+		"Apply the filter to the target python list")							\
+	.def ("Apply",				class##Apply,	args ("data"),					\
+		"Apply the filter to the target time series")							\
+	.def (self_ns::str (self_ns::self));
+
+//****************************************************************************//
+//      Common methods of the time series filters                             //
+//****************************************************************************//
+# define	FILTERS_COMMON(class) 												\
 	.add_property ("Size",		&class::Size, 									\
 		"Size of the impulse response function") 								\
 	.def ("Impulse",			&class::Impulse, 								\
 		return_internal_reference <> (), 										\
 		"Values of the impulse response function")								\
-	.def (self_ns::str (self_ns::self));
+	FILTERS_BASE(class)
 
 //****************************************************************************//
 //      Python module initialization functions                                //
@@ -36,48 +56,61 @@ BOOST_PYTHON_MODULE (filters) {
 //============================================================================//
 //      Expose "MedianFilter" class to Python                                 //
 //============================================================================//
-vector <double> (MedianFilter::*Apply1)(const pylist &py_list) const		= &MedianFilter::Apply;
-vector <double> (MedianFilter::*Apply2)(const vector <double> &data) const	= &MedianFilter::Apply;
+	POINTERS_COMMON(MedianFilter)
 	class_ <MedianFilter> ("MedianFilter",
 		"Median filter for a time series",
 		init <size_t> (args ("points"),
 			"Set count of neighbor points to filter by"))
-		.add_property ("Points",	&MedianFilter::Points,
-			"Count of neighbor points to filter by")
-		.def ("Apply",	Apply1,	args ("data"),
-			"Apply the filter to the target python list")
-		.def ("Apply",	Apply2,	args ("data"),
-			"Apply the filter to the target time series")
-		.def (self_ns::str (self_ns::self));
+		FILTERS_BASE(MedianFilter);
 
 //============================================================================//
 //      Expose "SmoothFilter" class to Python                                 //
 //============================================================================//
-vector <double> (SmoothFilter::*Apply3)(const pylist &py_list) const		= &SmoothFilter::Apply;
-vector <double> (SmoothFilter::*Apply4)(const vector <double> &data) const	= &SmoothFilter::Apply;
+	POINTERS_COMMON(SmoothFilter)
 	class_ <SmoothFilter> ("SmoothFilter",
 		"Smooth filter for a time series",
 		init <size_t> (args ("points"),
 			"Set count of neighbor points to smooth by"))
-		.def ("Apply",	Apply3,	args ("data"),
-			"Apply the filter to the target python list")
-		.def ("Apply",	Apply4,	args ("data"),
-			"Apply the filter to the target time series")
 		FILTERS_COMMON(SmoothFilter);
+
+//============================================================================//
+//      Expose "DiffFilter" class to Python                                   //
+//============================================================================//
+	POINTERS_COMMON(DiffFilter)
+	class_ <DiffFilter> ("DiffFilter",
+		"Differentiator filter for a time series",
+		init <size_t, double> (args ("points", "step"),
+			"Set count of neighbor points to smooth by and the discretization step"))
+		FILTERS_COMMON(DiffFilter);
+
+//============================================================================//
+//      Expose "SwingFilter" class to Python                                  //
+//============================================================================//
+	POINTERS_COMMON(SwingFilter)
+	class_ <SwingFilter> ("SwingFilter",
+		"Swing filter (no mean value) for a time series",
+		init <size_t> (args ("points"),
+			"Set count of neighbor points to smooth by"))
+		FILTERS_COMMON(SwingFilter);
+
+//============================================================================//
+//      Expose "HilbertFilter" class to Python                                //
+//============================================================================//
+	POINTERS_COMMON(HilbertFilter)
+	class_ <HilbertFilter> ("HilbertFilter",
+		"Hilbert transform filter for a time series",
+		init <size_t> (args ("size"),
+			"Set size of the dataset for the Hilbert transformation"))
+		FILTERS_COMMON(HilbertFilter);
 
 //============================================================================//
 //      Expose "MinDelayFilter" class to Python                               //
 //============================================================================//
-vector <double> (MinDelayFilter::*Apply5)(const pylist &py_list) const			= &MinDelayFilter::Apply;
-vector <double> (MinDelayFilter::*Apply6)(const vector <double> &data) const	= &MinDelayFilter::Apply;
+	POINTERS_COMMON(MinDelayFilter)
 	class_ <MinDelayFilter> ("MinDelayFilter",
 		"Minimum group delay filter for a time series",
 		init <size_t> (args ("points"),
 			"Set count of neighbor points to filter by"))
-		.def ("Apply",	Apply5,	args ("data"),
-			"Apply the filter to the target python list")
-		.def ("Apply",	Apply6,	args ("data"),
-			"Apply the filter to the target time series")
 		FILTERS_COMMON(MinDelayFilter);
 }
 /*
